b554cdf858db8b9ec5fb9260f86e2071fa96e454
cornelius <cornelius.lejeune@googlemail.com>
2025-10-24 18:13:52 +0200

    Implemented basic error handling for all network connections.

--- server.js
+++ server.js
@@ -54,49 +54,78 @@ app.get('/books/new', (req, res) => {
 })
 
 app.post('/books', async (req, res) => {
-    const submittedBook = req.body;
-    const newBook = await Book.create(submittedBook);
-    console.log(log.warning, `Stored new book ${newBook.titleText} as ${newBook._id}`);
-    res.redirect(`/books/${newBook._id}`);
+    try {
+        const submittedBook = req.body;
+        const newBook = await Book.create(submittedBook);
+        console.log(log.warning, `Stored new book ${newBook.titleText} as ${newBook._id}`);
+        res.redirect(`/books/${newBook._id}`);
+    } catch (error) {
+        const errorCode = 500;
+        res.status(errorCode).render('error', { err: { code: errorCode, msg: error }})
+    }
 })
 
 // Read book
 app.get('/books', async (req, res) => {
-    const retrievedBooks = await Book.find().lean();
-    console.log(log.info, `Retrieved all books`);
-    res.render('books/index', { books: retrievedBooks });
+    try {
+        const retrievedBooks = await Book.find();
+        console.log(log.info, `Retrieved all books`);
+        res.render('books/index', { books: retrievedBooks });
+    } catch (error) {
+        const errorCode = 500;
+        res.status(errorCode).render('error', { err: { code: errorCode, msg: error }})
+    }
 })
 
 app.get('/books/:id', async (req, res) => {
-    const bookId = req.params.id;
-    const retrievedBook = await Book.findById(bookId);
-    console.log(log.info, `Retrieved book ${retrievedBook.titleText} as ${retrievedBook._id}`);
-    res.render('books/show', { book: retrievedBook });
+    try {
+        const bookId = req.params.id;
+        const retrievedBook = await Book.findById(bookId);
+        console.log(log.info, `Retrieved book ${retrievedBook.titleText} as ${retrievedBook._id}`);
+        res.render('books/show', { book: retrievedBook });
+    } catch (error) {
+        const errorCode = 500;
+        res.status(errorCode).render('error', { err: { code: errorCode, msg: error }})
+    } 
 })
 
 // Edit book
 app.get('/books/:id/edit', async (req, res) => {
-    const bookId = req.params.id;
-    const retrievedBook = await Book.findById(bookId);
-    console.log(log.info, `Retrieved book ${retrievedBook.titleText} as ${retrievedBook._id}`);
-    res.render('books/edit', { book: retrievedBook });
+    try {
+        const bookId = req.params.id;
+        const retrievedBook = await Book.findById(bookId);
+        console.log(log.info, `Retrieved book ${retrievedBook.titleText} as ${retrievedBook._id}`);
+        res.render('books/edit', { book: retrievedBook });
+    } catch (error) {
+        const errorCode = 500;
+        res.status(errorCode).render('error', { err: { code: errorCode, msg: error }})
+    }
 })
 
 app.put('/books/:id', async (req, res) => {
-    const bookId = req.params.id;
-    const submittedBook = req.body;
-    const editedBook = await Book.findByIdAndUpdate(bookId, submittedBook);
-    console.log(log.warning, `Updated book ${editedBook.titleText} as ${editedBook._id}`);
-    res.redirect(`/books/${bookId}`);
+    try {
+        const bookId = req.params.id;
+        const submittedBook = req.body;
+        const editedBook = await Book.findByIdAndUpdate(bookId, submittedBook);
+        console.log(log.warning, `Updated book ${editedBook.titleText} as ${editedBook._id}`);
+        res.redirect(`/books/${bookId}`);
+    } catch (error) {
+        const errorCode = 500;
+        res.status(errorCode).render('error', { err: { code: errorCode, msg: error }})
+    }
 })
 
 // Delete book
 app.delete('/books/:id', async (req,res) => {
-    const bookId = req.params.id;
-    const deletedBook = await Book.findByIdAndDelete(bookId);
-    console.log(log.warning, `Deleted book ${deletedBook.titleText} as ${deletedBook._id}`);
-    res.redirect('/books');
-
+    try {
+        const bookId = req.params.id;
+        const deletedBook = await Book.findByIdAndDelete(bookId);
+        console.log(log.warning, `Deleted book ${deletedBook.titleText} as ${deletedBook._id}`);
+        res.redirect('/books');
+    } catch (error) {
+        const errorCode = 500;
+        res.status(errorCode).render('error', { err: { code: errorCode, msg: error }})
+    }
 })
 
 // ! Connections
